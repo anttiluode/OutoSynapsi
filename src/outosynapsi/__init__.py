e65289eb591ee735727f1473c003754a2503bfb1
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-09-03 17:24:54 +0300

    Add src/outosynapsi/__init__.py

---
+++ src/outosynapsi/__init__.py
@@ -0,0 +1,23 @@
+from .spectral_tree import (
+    Edge,
+    SpectralTree,
+    analytic_budget_oracle,
+    complete_binary_tree,
+    edge_flow,
+    mean_distance,
+    regularized_metric_objective,
+    stochastic_metric_plasticity,
+    traffic_proportional_allocation,
+)
+
+__all__ = [
+    "Edge",
+    "SpectralTree",
+    "analytic_budget_oracle",
+    "complete_binary_tree",
+    "edge_flow",
+    "mean_distance",
+    "regularized_metric_objective",
+    "stochastic_metric_plasticity",
+    "traffic_proportional_allocation",
+]
